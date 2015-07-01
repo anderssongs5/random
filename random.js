342d039398472a524900766d88c52ee7581d3dc2
anderssongs5 <anderssongarciasotelo@gmail.com>
2015-06-30 22:02:46 -0500

    Mejorando implementación

--- random.js
+++ random.js
@@ -1,7 +1,10 @@
 var number = Math.random();
+var message = '';
 
 if(number <= 0.5){
-  console.log(number + ":número aleatorio menor o igual que 0.5\n");
+  message = "número aleatorio menor o igual que 0.5\n");
 } else {
-  console.log(number + ":número aleatorio mayor que 0.5\n");
+  message = "número aleatorio mayor que 0.5\n");
 }
+
+console.log(number + message);
