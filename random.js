886a06070c21c2145d85890c9db305545f2d5a70
anderssongs5 <anderssongarciasotelo@gmail.com>
2015-06-30 21:55:22 -0500

    Commit inicial
    * Se añade archivo JS donde se genera número random y se indica si es mayor o menor a uno de referencia.
    * Se agrega el archivo .gitignore

---
+++ random.js
@@ -0,0 +1,7 @@
+var number = Math.random();
+
+if(number <= 0.5){
+  console.log(number + ":número aleatorio menor o igual que 0.5\n");
+} else {
+  console.log(number + ":número aleatorio mayor que 0.5\n");
+}
